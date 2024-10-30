864961a8ec52826ba1fbc5af75acd1ccc4c895be
Joachim Renevier <renevierjoachim@gmail.com>
2024-10-30 10:49:48 +0100

    init sql

--- Makefile
+++ Makefile
@@ -1,2 +1,20 @@
-buildbackend:
-	docker compose backend up --build
+# Makefile
+DOCKER_COMPOSE_FILE=docker-compose.yml
+SERVICE_NAME=my_service_name 
+
+.PHONY: start stop restart build
+
+# Commande pour démarrer les conteneurs
+start:
+    docker compose -f $(DOCKER_COMPOSE_FILE) up -d
+
+# Commande pour arrêter les conteneurs
+stop:
+    docker compose -f $(DOCKER_COMPOSE_FILE) down
+
+# Commande pour redémarrer les conteneurs
+restart: stop start
+
+# Commande pour reconstruire les conteneurs
+build:
+    docker compose -f $(DOCKER_COMPOSE_FILE) up -d --build
---
+++ sql/init.sql
@@ -0,0 +1,51 @@
+-- Utilisation de la base de données
+use app;
+
+-- Création de la table `user`
+create table if not exists user (
+    id int primary key auto_increment,
+    login varchar(255) not null,
+    password varchar(255) not null,
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp on update current_timestamp
+);
+
+-- Création de la table `file`
+create table if not exists `file` (
+    id int primary key auto_increment,
+    data varchar(255) not null,
+    title varchar(255) not null,
+    description text,
+    user_id int not null,
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp on update current_timestamp,
+    foreign key (user_id) references `user`(id)
+);
+
+-- Création de la table `share`
+create table if not exists share (
+    id int primary key auto_increment,
+    user_id int,
+    file_id int not null,
+    created_at timestamp default current_timestamp not null,
+    updated_at timestamp default current_timestamp on update current_timestamp not null,
+    link varchar(255) not null,
+    expired_at timestamp,
+    foreign key (user_id) references `user`(id),
+    foreign key (file_id) references `file`(id)
+);
+
+
+-- Insertion de données dans `user`
+insert into `user` (login, password) values ('roger', 'roger');
+insert into `user` (login, password) values ('jean', 'jean');
+
+-- Insertion de données dans `file`
+insert into `file` (data, title, description, user_id) values ('file1', 'file1', 'file1', 1);
+insert into `file` (data, title, description, user_id) values ('file2', 'file2', 'file2', 2);
+
+-- Insertion de données dans `share`
+insert into share (user_id, file_id, link) values (1, 1, 'file1');
+insert into share (user_id, file_id, link) values (2, 2, 'file2');
+
+
