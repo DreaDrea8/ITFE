40be34bd66647acc8b47c0e8946ed796960aad31
Sylvain <sylvainavalle@gmail.com>
2024-10-30 10:56:06 +0100

    up

--- README.md
+++
@@ -1,7 +0,0 @@
-# ITFE
-Same then S3 WeTransfere 
-
-#
-Thecno utiliser 
-Fronte : React
-Back : nodeJs
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 import Root from "./routes/root";
 import ErrorPage from "./error-page";
-import SignUp from "./routes/SignUp.tsx";
-import SignIn from "./routes/SignIn.tsx";
+import SignUp from "./routes/signup";
+import SignIn from "./routes/signin";
 
 const router = createBrowserRouter([
   {
